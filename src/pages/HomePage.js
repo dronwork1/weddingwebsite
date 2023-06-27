0ae7fcfce243a63d2cff09f9a5a202fb8cc4774a
alyonanov <unixcats@gmail.com>
2023-06-28 00:09:11 +0300

    make page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import './HomePage.css'
+const HomePage =() => {
+    return(
+        <div >
+            <div className="homepage">
+            <p className='homePageText'>
+                Совсем скоро 
+            </p>
+            <div className='oval'>
+            <p className='homePageText'>
+                тут
+            </p>
+            </div>
+            <p className='homePageText'>
+                что-то будет...
+            </p>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage;
